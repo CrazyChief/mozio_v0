6ef34835429c6ca8f16cefa627591b1102f5f945
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-05-17 12:03:53 +0300

    Provide models and admin integration

--- mozio_v0/providers/admin.py
+++ mozio_v0/providers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Provider, ServiceArea
+
+
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number', 'language', 'currency']
+
+
+@admin.register(ServiceArea)
+class ServiceAreaAdmin(admin.ModelAdmin):
+    list_display = ['polygon_name', 'price']
