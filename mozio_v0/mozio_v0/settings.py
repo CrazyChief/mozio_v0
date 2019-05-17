b600feeffc3c6503b1abfc5b6f4bd40ef64c0e57
Dmitry Danilov <danilovdmitry94@gmail.com>
2019-05-17 12:30:56 +0300

    Added DRF config

--- mozio_v0/mozio_v0/settings.py
+++ mozio_v0/mozio_v0/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api_v0',
     'djmoney',
     'languages',
@@ -135,3 +136,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Setting for phonefield
+PHONENUMBER_DB_FORMAT = 'INTERNATIONAL'
+
+# REST Framework settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100
+}
